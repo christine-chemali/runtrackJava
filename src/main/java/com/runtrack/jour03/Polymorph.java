1f788ac365684abdafef07240a67a2b675b29955
christine chemali <christine.chemali@laplateforme.io>
2025-06-26 12:24:01 +0200

    Day 03 job 03 questions 1 and 2 answers

--- src/main/java/com/runtrack/jour03/Polymorph.java
+++ src/main/java/com/runtrack/jour03/Polymorph.java
@@ -14,7 +14,7 @@
  * 2 . Qu'affiche t'il ?
  * 3 . Corriger ce programme de sorte qu'il fasse ce que le programmeur
  * voulait à l'origine. Donner le code Java de tout élément ajouté 
- * en précisant à quells classe il appartient et indiquerez les 
+ * en précisant à quelles classe il appartient et indiquerez les 
  * éventuelles modifications à apporter à la méthode main.
  * 
  * lES CONTRAINTES A RESPECTER SONT LES SUIVANTES
@@ -120,3 +120,39 @@ class Collect
         collect[i].dessine();
     }
 }
+
+/*
+ * Réponses :
+ * 1. Expliquer pourquoi ce programme principal ne fait pas 
+ * réellement ce qu'il veux.
+ * 
+ * Le problème principal se situe dans la méthode main:
+ * ICI -> formes.add(new Forme(tabFormes[i]));
+ * le programmeur crée de nouveaux objets de type "Forme" en
+ * utilisant le constructeur de copie, mais il perd ainsi le type
+ * spécifique des objets originaux "Cercle" et "Triangle".
+ * Les objets copiés deviennent des instances 
+ * de la classe "Forme" de base, en perdant 
+ * leur comportement polymorphe.
+ * 
+ * 2 . Qu'affiche t'il ?
+ * 
+ * Il affiche :
+ * 
+ * Une forme rouge
+ * Une forme jaune
+ * 
+ * Parce que les objets copiés sont de type
+ * "Forme" et non plus "Cercle" ou "Triangle"
+ * Seule la méthode "dessine()" de la classe "Forme" est
+ * appelée.
+ * 
+ * 3 . Corriger ce programme de sorte qu'il fasse ce que le programmeur
+ * voulait à l'origine. Donner le code Java de tout élément ajouté 
+ * en précisant à quelles classe il appartient et indiquerez les 
+ * éventuelles modifications à apporter à la méthode main.
+ * 
+ * Cela sera fait dans le code 
+ * en commentant ce qui doit être changé ou supprimé
+ * 
+ */
