c2953ac930dce8b4488042f4f20c091c05ba3820
christine chemali <christine.chemali@laplateforme.io>
2025-06-29 09:52:03 +0200

    Jour 05 job 05 retout console part 1 - imports

---
+++ src/main/java/com/runtrack/jour05/job05App.java
@@ -0,0 +1,30 @@
+/* JOUR 05 JOB 05 RUNTRACK JAVA
+ * Dans ce job, l'objectif est d'ajouter un évenement au bouton
+ * "Envoyer" pour afficher le nom entré dans le champ de texte dans la console
+ * Le but est de comprendre comment utiliser les évenements en java fx
+ * pour répondre aux actions de l'utilisateur
+ * 
+ * Résultat attendu
+ * -> lorsque le bouton "Envoyer" est cliqué, le nom entré dans le champ
+ * de texte doit être affiché dans la console
+ * 
+ */
+
+ package com.runtrack.jour05;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class job05App extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        
+    }
+    
+}
